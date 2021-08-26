0bb2cc8adb42587cf7871576c6a43c5fa68737d0
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-08-26 12:57:44 +0530

    multiple pages

--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-print("hello")
+
+
+def january (request):
+    return HttpResponse("January")
+
+def february (request):
+    return HttpResponse("February")
+
+def march (request):
+    return HttpResponse("March")
