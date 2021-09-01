ed43c3ae775cd9eb78120d6b3f9c9cfdff955e6c
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-09-01 15:42:44 +0530

    another change in dynamic redirect path

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january":"Eat no meat",
