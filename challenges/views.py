9d549322874e2a9f6798d8ae629ad2ed80bc1a78
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-09-01 11:10:04 +0530

    multi pages in single function

--- challenges/views.py
+++ challenges/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january (request):
-    return HttpResponse("January")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat"
+    elif month == "february":
+        challenge_text = "walk for 20 mins"
+    elif month == "march":
+        challenge_text = "Learn Django"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
 
-def february (request):
-    return HttpResponse("February")
-
-def march (request):
-    return HttpResponse("March")
