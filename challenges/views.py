d15d90290c454d7191e4d2a9e3d90ca7abe15c71
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-09-01 15:39:15 +0530

    dynamic redirect path

--- challenges/views.py
+++ challenges/views.py
@@ -24,7 +24,8 @@ def monthly_challenge_by_number(request,month):
         return HttpResponseNotFound("Invalid month")
     
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args = [redirect_month]) # //challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
